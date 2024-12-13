2bb89812a8d840eabce612107ee8580959ee7296
Istiyak Tailor <tailoristiyak303@gmail.com>
2024-12-12 22:53:24 -0500

    feat: changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,128 +1,146 @@
-const AppointmentInfo = require('../models/AppointmentInfo.js');
-const DriverTestInfo = require('../models/DriverTestInfo.js');
-const OrderInfo = require('../models/OrderInfo.js');
+const AppointmentInfo = require("../models/AppointmentInfo.js");
+const DriverTestInfo = require("../models/DriverTestInfo.js");
+const OrderInfo = require("../models/OrderInfo.js");
 
 module.exports = {
-    //路由到预约页面
-    routeAppointment: (req, res) => {
-        if (req.session.driverInfo?.UserType !== 'Admin') {
-            return res.redirect('/');
-        }
-        var date = '';
-        var time = '';
-        const data = req.flash('data')[0];
+  //路由到预约页面
+  routeAppointment: (req, res) => {
+    if (req.session.driverInfo?.UserType !== "Admin") {
+      return res.redirect("/");
+    }
 
-        if (typeof data != "undefined") {
-            date = data.date;
-            time = data.time;
-            console.log('Appointment request param: ', { date, time });
-        }
+    var date = "";
+    var time = "";
+    const data = req.flash("data")[0];
 
-        res.render('adminAppointment', {
-            date: date,
-            time: time,
-            errors: req.flash('validationErrors'),
-            success: req.flash('success')
+    if (typeof data != "undefined") {
+      date = data.date;
+      time = data.time;
+      console.log("Appointment request param: ", { date, time });
+    }
+
+    res.render("adminAppointment", {
+      date: date,
+      time: time,
+      errors: req.flash("validationErrors"),
+      success: req.flash("success"),
+    });
+  },
+
+  //查询预约信息
+  retrievalAppointment: async (req, res) => {
+    try {
+      console.log("Get appointment page start.");
+      const { date } = req.query;
+      const appointments = await AppointmentInfo.find({ date: date });
+      res.json(appointments);
+      console.log("Get appointment page end.");
+    } catch (error) {
+      console.error("Error fetching appointments:", error);
+      res.status(500).send("Error fetching appointments.");
+    }
+  },
+
+  //添加预约信息
+  addAppointment: async (req, res) => {
+    try {
+      const { date, time } = req.body;
+
+      // 检查是否已存在
+      const existingAppointment = await AppointmentInfo.findOne({ date, time });
+
+      if (existingAppointment) {
+        return res.json({
+          success: false,
+          message: `Time slot already exists for date: ${date}, time: ${time}`,
         });
-    },
+      }
 
-    //查询预约信息
-    retrievalAppointment: async (req, res) => {
-        try {
-            console.log("Get appointment page start.");
-            const { date } = req.query;
-            const appointments = await AppointmentInfo.find({ date: date });
-            res.json(appointments);
-            console.log("Get appointment page end.");
-        } catch (error) {
-            console.error('Error fetching appointments:', error);
-            res.status(500).send('Error fetching appointments.');
-        }
-    },
+      // 添加新 time-slot
+      const newAppointment = new AppointmentInfo({
+        date,
+        time,
+        isTimeSlotAvailable: true,
+      });
 
-    //添加预约信息
-    addAppointment: async (req, res) => {
-        try {
-            console.log("Add appointment page start.");
-            const { date, time } = req.body;
+      await newAppointment.save();
 
-            // 检查是否已存在
-            const existingAppointment = await AppointmentInfo.findOne({ date, time });
-            if (existingAppointment) {
-                return res.json({ success: false, message: `Time slot already exists for date: ${date}, time: ${time}` });
-            }
+      res.json({ success: true, message: "Time-slot added successfully!" });
+    } catch (error) {
+      console.error("Error adding time-slot:", error);
+      res
+        .status(500)
+        .json({ success: false, message: "Error adding time-slot." });
+    }
+  },
 
-            // 添加新 time-slot
-            const newAppointment = new AppointmentInfo({
-                date,
-                time,
-                isTimeSlotAvailable: true,
-            });
+  // 获取候选人列表
+  getCandidates: async (req, res) => {
+    try {
+      // 查询所有候选人，并排除 LicenseNo 为 'default' 的候选人
+      const candidates = await DriverTestInfo.find({
+        UserType: "Driver",
+        LicenseNo: { $ne: "default" },
+      });
 
-            await newAppointment.save();
+      // 查询所有订单，避免重复创建订单
+      const orders = await OrderInfo.find();
+      const orderedCandidateIds = orders.map((order) =>
+        order.candidateId.toString()
+      );
 
-            res.json({ success: true, message: 'Time-slot added successfully!' });
-        } catch (error) {
-            console.error('Error adding time-slot:', error);
-            res.status(500).json({ success: false, message: 'Error adding time-slot.' });
-        }
-    },
+      // 将订单状态附加到候选人信息
+      const candidatesWithOrderStatus = candidates.map((candidate) => ({
+        ...candidate.toObject(),
+        hasOrder: orderedCandidateIds.includes(candidate._id.toString()),
+      }));
 
-    // 获取候选人列表
-    getCandidates: async (req, res) => {
-        try {
-            // 查询所有候选人，并排除 LicenseNo 为 'default' 的候选人
-            const candidates = await DriverTestInfo.find({ UserType: 'Driver', LicenseNo: { $ne: 'default' } });
+      res.render("adminCandidate", {
+        candidates: candidatesWithOrderStatus,
+        success: req.flash("success"),
+        errors: req.flash("validationErrors"),
+      });
+    } catch (error) {
+      console.error("Error fetching candidates:", error);
+      req.flash("validationErrors", ["Error fetching candidates"]);
+      res.redirect("/admin/candidate");
+    }
+  },
 
-            // 查询所有订单，避免重复创建订单
-            const orders = await OrderInfo.find();
-            const orderedCandidateIds = orders.map(order => order.candidateId.toString());
+  // 创建订单
+  createOrder: async (req, res) => {
+    try {
+      const { candidateId } = req.body;
+      const candidate = await DriverTestInfo.findById(candidateId);
 
-            // 将订单状态附加到候选人信息
-            const candidatesWithOrderStatus = candidates.map(candidate => ({
-                ...candidate.toObject(),
-                hasOrder: orderedCandidateIds.includes(candidate._id.toString()),
-            }));
+      if (!candidate || candidate.TestResult !== "PASS") {
+        req.flash("validationErrors", [
+          "Invalid candidate or candidate not passed",
+        ]);
+        return res.redirect("/admin/candidate");
+      }
 
-            res.render('adminCandidate', {
-                candidates: candidatesWithOrderStatus,
-                success: req.flash('success'),
-                errors: req.flash('validationErrors'),
-            });
-        } catch (error) {
-            console.error('Error fetching candidates:', error);
-            req.flash('validationErrors', ['Error fetching candidates']);
-            res.redirect('/admin/candidate');
-        }
-    },
+      // 检查是否已存在订单
+      const existingOrder = await OrderInfo.findOne({ candidateId });
+      if (existingOrder) {
+        req.flash("validationErrors", [
+          "Order already exists for this candidate",
+        ]);
+        return res.redirect("/admin/candidate");
+      }
 
-    // 创建订单
-    createOrder: async (req, res) => {
-        try {
-            const { candidateId } = req.body;
-            const candidate = await DriverTestInfo.findById(candidateId);
+      // 创建新订单
+      const order = new OrderInfo({ candidateId, orderType: "Driver License" });
+      await order.save();
 
-            if (!candidate || candidate.TestResult !== 'PASS') {
-                req.flash('validationErrors', ['Invalid candidate or candidate not passed']);
-                return res.redirect('/admin/candidate');
-            }
-
-            // 检查是否已存在订单
-            const existingOrder = await OrderInfo.findOne({ candidateId });
-            if (existingOrder) {
-                req.flash('validationErrors', ['Order already exists for this candidate']);
-                return res.redirect('/admin/candidate');
-            }
-
-            // 创建新订单
-            const order = new OrderInfo({ candidateId, orderType: 'Driver License' });
-            await order.save();
-
-            req.flash('success', `Order created for ${candidate.firstname} ${candidate.lastname}`);
-        } catch (error) {
-            console.error('Error creating order:', error);
-            req.flash('validationErrors', ['Error creating order']);
-        }
-        res.redirect('/admin/candidate');
-    },
+      req.flash(
+        "success",
+        `Order created for ${candidate.firstname} ${candidate.lastname}`
+      );
+    } catch (error) {
+      console.error("Error creating order:", error);
+      req.flash("validationErrors", ["Error creating order"]);
+    }
+    res.redirect("/admin/candidate");
+  },
 };
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,221 +1,237 @@
-'use strict'
+"use strict";
 
 const timeSlots = [
-    "9:00", "9:30", "10:00", "10:30", "11:00", "11:30",
-    "12:00", "12:30", "13:00", "13:30", "14:00"
+  "9:00",
+  "9:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
 ];
 
 const datePicker = (dateInput) => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
-    const formattedDate = `${year}-${month}-${day}`;
-    dateInput.attr('min', formattedDate);
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  dateInput.attr("min", formattedDate);
 };
 
 const adminCheckTimeSlot = () => {
-    $('#adminDate').on('change', function () {
-        const date = $(this).val();
-        console.log('Date was changed:', date);
+  $("#adminDate").on("change", function () {
+    const date = $(this).val();
+    console.log("Date was changed:", date);
 
-        // 清空之前的按钮
-        $('#adminTimeButtons').empty();
+    // 清空之前的按钮
+    $("#adminTimeButtons").empty();
 
-        if (!date) return;
+    if (!date) return;
 
-        // 请求服务器获取时间槽数据
-        $.ajax({
-            url: '/admin/appointment/retrieval',
-            method: 'GET',
-            data: { date },
-            success: function (response) {
-                console.log('Response from server:', response); // 打印服务器返回数据
+    // 请求服务器获取时间槽数据
+    $.ajax({
+      url: "/admin/appointment/retrieval",
+      method: "GET",
+      data: { date },
+      success: function (response) {
+        console.log("Response from server:", response); // 打印服务器返回数据
 
-                if (response.length === 0) {
-                    console.log('No time slots available for the selected date.');
-                    timeSlots.forEach(time => {
-                        const button = $(`<button class="time-slot available" type="button">${time}</button>`);
-                        $("#adminTimeButtons").append(button);
+        if (response.length === 0) {
+          console.log("No time slots available for the selected date.");
+          timeSlots.forEach((time) => {
+            const button = $(
+              `<button class="time-slot available" type="button">${time}</button>`
+            );
+            $("#adminTimeButtons").append(button);
 
-                         // 绑定点击事件
-                         button.on('click', event => {
-                            event.preventDefault();
-                            $('.time-slot').removeClass('selected');
-                            $(event.target).addClass('selected');
-                            $('#selectedTime').val(time); // 更新选中的时间
-                        });
-                    });
-                    return;
-                }
+            // 绑定点击事件
+            button.on("click", (event) => {
+              event.preventDefault();
+              $(".time-slot").removeClass("selected");
+              $(event.target).addClass("selected");
+              $("#selectedTime").val(time); // 更新选中的时间
+            });
+          });
+          return;
+        }
 
-                // 遍历所有时间槽
-                timeSlots.forEach(time => {
-                    const slot = response.find(slot => slot.time === time);
-                    const isAvailable = slot ? false : true;
+        // 遍历所有时间槽
+        timeSlots.forEach((time) => {
+          const slot = response.find((slot) => slot.time === time);
+          const isAvailable = slot ? false : true;
 
-                    // 创建按钮
-                    const button = $('<button>')
-                        .addClass('time-slot')
-                        .addClass(isAvailable ? 'available' : 'unavailable')
-                        .prop('disabled', !isAvailable)
-                        .attr('type', 'button')
-                        .text(time);
+          // 创建按钮
+          const button = $("<button>")
+            .addClass("time-slot")
+            .addClass(isAvailable ? "available" : "unavailable")
+            .prop("disabled", !isAvailable)
+            .attr("type", "button")
+            .text(time);
 
-                    // 检查是否需要设置为 selected
-                    const selectedTime = $('#selectedTime').val();
+          // 检查是否需要设置为 selected
+          const selectedTime = $("#selectedTime").val();
 
-                    if (isAvailable && selectedTime === time) {
-                        console.log(`Setting button as selected: ${time}`);
-                        button.addClass('selected');
-                    }
+          if (isAvailable && selectedTime === time) {
+            console.log(`Setting button as selected: ${time}`);
+            button.addClass("selected");
+          }
 
-                    // 绑定点击事件
-                    if (isAvailable) {
-                        button.on('click', event => {
-                            event.preventDefault();
-                            $('.time-slot').removeClass('selected');
-                            $(event.target).addClass('selected');
-                            $('#selectedTime').val(time); // 更新选中的时间
-                        });
-                    }
+          // 绑定点击事件
+          if (isAvailable) {
+            button.on("click", (event) => {
+              event.preventDefault();
+              $(".time-slot").removeClass("selected");
+              $(event.target).addClass("selected");
+              $("#selectedTime").val(time); // 更新选中的时间
+            });
+          }
 
-                    // console.log('Generated button content:', button.prop('outerHTML'));
+          // console.log('Generated button content:', button.prop('outerHTML'));
 
-                    // 将按钮添加到页面
-                    $('#adminTimeButtons').append(button);
-                });
-            },
-            error: function () {
-                alert('Failed to fetch time slots. Please try again.');
-            }
+          // 将按钮添加到页面
+          $("#adminTimeButtons").append(button);
         });
+      },
+      error: function () {
+        alert("Failed to fetch time slots. Please try again.");
+      },
     });
+  });
 };
 
 const sotreAppointment = () => {
-    $('#postAppointmentForm').on('submit', function (e) {
-        e.preventDefault(); // 阻止表单的默认提交行为
+  $("#postAppointmentForm").on("submit", function (e) {
+    e.preventDefault(); // 阻止表单的默认提交行为
 
-        const formData = $(this).serialize(); // 获取表单数据
+    const formData = $(this).serialize(); // 获取表单数据
 
-        $.ajax({
-            url: '/admin/appointment/add', // 替换为实际后端路径
-            method: 'POST',
-            data: formData,
-            success: function (response) {
-                if (response.success) {
-                    alert(response.message);
+    $.ajax({
+      url: "/admin/appointment/add", // 替换为实际后端路径
+      method: "POST",
+      data: formData,
+      success: function (response) {
+        if (response.success) {
+          showBootstrapAlert(response.message, "success");
 
-                    // 清空选中时间
-                    $('#selectedTime').val('');
+          // 清空选中时间
+          $("#selectedTime").val("");
 
-                    // 刷新时间槽按钮
-                    $('#adminDate').trigger('change'); // 模拟日期变更以刷新按钮
-                } else {
-                    alert(response.message);
-                }
-            },
-            error: function () {
-                alert('Failed to add time-slot. Please try again.');
-            }
-        });
+          // 刷新时间槽按钮
+          $("#adminDate").trigger("change"); // 模拟日期变更以刷新按钮
+        } else {
+          showBootstrapAlert(response.message, "warning");
+        }
+      },
+      error: function () {
+        showBootstrapAlert(
+          "Failed to add time-slot. Please try again.",
+          "danger"
+        );
+      },
     });
+  });
 };
 
 const queryTimeSlot = () => {
-    let selectedTimeId = null;
+  let selectedTimeId = null;
 
-    $('#date').on('change', function () {
-        const date = $(this).val();
-        console.log('date was changed : ' + date);
-        if (!date) return;
+  $("#date").on("change", function () {
+    const date = $(this).val();
+    console.log("date was changed : " + date);
+    if (!date) return;
 
-        // Clear previous buttons
-        $('#timeButtons').empty();
+    // Clear previous buttons
+    $("#timeButtons").empty();
 
-        // Fetch available slots from the server
-        $.ajax({
-            url: '/admin/appointment/retrieval',
-            method: 'GET',
-            data: { date },
-            success: function (response) {
-                if (response.length === 0) {
-                    $('#timeButtons').html('<p>No available slots for this date.</p>');
-                    return;
-                }
-                $('#timeButtons').append('<br>');
-                response.forEach(slot => {
-                    if (!slot.isTimeSlotAvailable) {
-                        return;
-                    }
-                    const button = $('<button>')
-                        .addClass('time-slot')
-                        .text(slot.time)
-                        .prop('disabled', !slot.isTimeSlotAvailable)
-                        .toggleClass('booked', !slot.isTimeSlotAvailable)
-                        .data('id', slot._id);
+    // Fetch available slots from the server
+    $.ajax({
+      url: "/admin/appointment/retrieval",
+      method: "GET",
+      data: { date },
+      success: function (response) {
+        if (response.length === 0) {
+          $("#timeButtons").html("<p>No available slots for this date.</p>");
+          return;
+        }
+        $("#timeButtons").append("<br>");
+        response.forEach((slot) => {
+          if (!slot.isTimeSlotAvailable) {
+            return;
+          }
+          const button = $("<button>")
+            .addClass("time-slot")
+            .text(slot.time)
+            .prop("disabled", !slot.isTimeSlotAvailable)
+            .toggleClass("booked", !slot.isTimeSlotAvailable)
+            .data("id", slot._id);
 
-                    // Handle button click
-                    button.on('click', function (event) {
-                        event.preventDefault();
-                        $('.time-slot').removeClass('selected');
-                        $(this).addClass('selected');
-                        selectedTimeId = $(this).data('id');
-                        $('#AppointmentId').val(selectedTimeId);
-                    });
+          // Handle button click
+          button.on("click", function (event) {
+            event.preventDefault();
+            $(".time-slot").removeClass("selected");
+            $(this).addClass("selected");
+            selectedTimeId = $(this).data("id");
+            $("#AppointmentId").val(selectedTimeId);
+          });
 
-                    $('#timeButtons').append(button);
-                });
-            },
-            error: function () {
-                alert('Failed to fetch time slots. Please try again.');
-            }
+          $("#timeButtons").append(button);
         });
-    })
+      },
+      error: function () {
+        alert("Failed to fetch time slots. Please try again.");
+      },
+    });
+  });
 };
 
 const retrievalDriverInfos = () => {
-    // 绑定 Filter 按钮的提交事件
-    $('#examinerGetForm').on('submit', function (e) {
-        e.preventDefault(); // 阻止表单默认提交
+  // 绑定 Filter 按钮的提交事件
+  $("#examinerGetForm").on("submit", function (e) {
+    e.preventDefault(); // 阻止表单默认提交
 
-        // 获取表单数据
-        const formData = $(this).serialize();
+    // 获取表单数据
+    const formData = $(this).serialize();
 
-        // AJAX 请求后端接口
-        $.ajax({
-            url: '/examiner/driverInfo/retrieval',
-            type: 'GET',
-            data: formData,
-            success: function (result) {
-                if (result.success) {
-                    renderDriverInfos(result.driverInfos); // 动态更新表格
-                } else {
-                    alert('Failed to retrieve data: ' + result.message);
-                }
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', error);
-                alert('An error occurred: ' + error);
-            },
-        });
+    // AJAX 请求后端接口
+    $.ajax({
+      url: "/examiner/driverInfo/retrieval",
+      type: "GET",
+      data: formData,
+      success: function (result) {
+        if (result.success) {
+          renderDriverInfos(result.driverInfos); // 动态更新表格
+        } else {
+          alert("Failed to retrieve data: " + result.message);
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("Error:", error);
+        alert("An error occurred: " + error);
+      },
     });
+  });
 };
 
 const renderDriverInfos = (driverInfos) => {
-    const $tbody = $('table tbody');
-    $tbody.empty(); // 清空表格
+  const $tbody = $("table tbody");
+  $tbody.empty(); // 清空表格
 
-    // 遍历 driverInfos 并动态生成表格行
-    driverInfos.forEach((driverInfo) => {
-        let actionCell = '';
+  // 遍历 driverInfos 并动态生成表格行
+  driverInfos.forEach((driverInfo) => {
+    let actionCell = "";
 
-        // 如果 testResult 为 PENDING，则显示操作表单
-        if (driverInfo.TestResult === 'PENDING') {
-            actionCell = `
+    // 如果 testResult 为 PENDING，则显示操作表单
+    if (driverInfo.TestResult === "PENDING") {
+      actionCell = `
                     <form class="updateForm" data-id="${driverInfo._id}">
-                        <textarea name="comment" placeholder="Add comment" required>${driverInfo.Comment || ''}</textarea>
+                        <textarea name="comment" placeholder="Add comment" required>${
+                          driverInfo.Comment || ""
+                        }</textarea>
                         <select name="testResult" required>
                             <option value="PASS">PASS</option>
                             <option value="FAIL">FAIL</option>
@@ -223,16 +239,16 @@ const renderDriverInfos = (driverInfos) => {
                         <button type="submit" class="btn btn-primary">Update</button>
                     </form>
                 `;
-        } else {
-            // 如果 testResult 已确定，则显示不可修改的内容
-            actionCell = `
-                    <span>Test Result: ${driverInfo.TestResult || ''}</span>
-                    <p>Comment: ${driverInfo.Comment || ''}</p>
+    } else {
+      // 如果 testResult 已确定，则显示不可修改的内容
+      actionCell = `
+                    <span>Test Result: ${driverInfo.TestResult || ""}</span>
+                    <p>Comment: ${driverInfo.Comment || ""}</p>
                 `;
-        }
+    }
 
-        // 动态生成行
-        const $row = $(`
+    // 动态生成行
+    const $row = $(`
                 <tr>
                     <td>${driverInfo.firstname} ${driverInfo.lastname}</td>
                     <td>${driverInfo.LicenseNo}</td>
@@ -242,58 +258,77 @@ const renderDriverInfos = (driverInfos) => {
                 </tr>
             `);
 
-        $tbody.append($row);
-    });
+    $tbody.append($row);
+  });
 
-    // 绑定更新按钮的提交事件
-    bindUpdateEvents();
+  // 绑定更新按钮的提交事件
+  bindUpdateEvents();
 };
 
 const bindUpdateEvents = () => {
-    $('.updateForm').on('submit', function (e) {
-        e.preventDefault(); // 阻止表单默认提交
+  $(".updateForm").on("submit", function (e) {
+    e.preventDefault(); // 阻止表单默认提交
 
-        const $form = $(this);
-        const driverId = $form.data('id');
-        const formData = $form.serialize();
+    const $form = $(this);
+    const driverId = $form.data("id");
+    const formData = $form.serialize();
 
-        // AJAX 请求更新接口
-        $.ajax({
-            url: `/examiner/driverInfo/update/${driverId}`,
-            type: 'POST',
-            data: formData,
-            success: function (result) {
-                if (result.success) {
-                    alert('Update successful!');
-                    $form.closest('tr').find('td:last-child').html(`
-                        <span>Test Result: ${result.updatedDriver.TestResult}</span>
-                        <p>Comment: ${result.updatedDriver.Comment || 'No comment'}</p>
+    // AJAX 请求更新接口
+    $.ajax({
+      url: `/examiner/driverInfo/update/${driverId}`,
+      type: "POST",
+      data: formData,
+      success: function (result) {
+        if (result.success) {
+          alert("Update successful!");
+          $form.closest("tr").find("td:last-child").html(`
+                        <span>Test Result: ${
+                          result.updatedDriver.TestResult
+                        }</span>
+                        <p>Comment: ${
+                          result.updatedDriver.Comment || "No comment"
+                        }</p>
                     `);
-                } else {
-                    alert('Failed to update: ' + result.message);
-                }
-            },
-            error: function (xhr, status, error) {
-                console.error('Error:', error);
-                alert('An error occurred: ' + error);
-            },
-        });
+        } else {
+          alert("Failed to update: " + result.message);
+        }
+      },
+      error: function (xhr, status, error) {
+        console.error("Error:", error);
+        alert("An error occurred: " + error);
+      },
     });
+  });
+};
+
+function showBootstrapAlert(message, type = "success") {
+  const alertContainer = $("#alertContainer");
+  const alertHtml = `
+        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `;
+  alertContainer.html(alertHtml);
+
+  // Optionally, auto-dismiss after a few seconds
+  setTimeout(() => {
+    $(".alert").alert("close");
+  }, 5000);
 }
 
 $(document).ready(() => {
+  datePicker($("#date"));
+  datePicker($("#adminDate"));
 
-    datePicker($('#date'));
-    datePicker($('#adminDate'));
+  if ($("#postAppointmentForm").val() != undefined) {
+    adminCheckTimeSlot();
+    sotreAppointment();
+  }
 
-    if ($('#postAppointmentForm').val() != undefined) {
-        adminCheckTimeSlot();
-        sotreAppointment();
-    }
+  if ($("#AppointmentId").val() != undefined) {
+    queryTimeSlot();
+  }
 
-    if ($('#AppointmentId').val() != undefined) {
-        queryTimeSlot();
-    }
-
-    retrievalDriverInfos();
+  retrievalDriverInfos();
 });
